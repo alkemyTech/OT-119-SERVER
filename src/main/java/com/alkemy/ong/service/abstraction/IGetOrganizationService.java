187203873fade3090729ffc05e001b4ce29af2f9
Pedro Asán Martínez <94468975+asanmartinezp@users.noreply.github.com>
2021-12-17 15:53:51 -0300

    OT119-15: implemented GET /organization/public endpoint (#1)

---
+++ src/main/java/com/alkemy/ong/service/abstraction/IGetOrganizationService.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.service.abstraction;
+
+import com.alkemy.ong.model.entity.Organization;
+
+public interface IGetOrganizationService {
+
+  Organization getOrganization();
+
+}
