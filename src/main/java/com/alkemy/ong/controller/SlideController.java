d6e2163fda6c74c8caca7b043492cb9fa2808890
Mauricio Ferrari <mauricioferrari239@gmail.com>
2021-12-19 12:49:29 -0300

    Agregado el controller que devuelve todos los slides

--- src/main/java/com/alkemy/ong/controller/SlideController.java
+++ src/main/java/com/alkemy/ong/controller/SlideController.java
@@ -1,12 +1,16 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.model.response.SlideResponse;
 import com.alkemy.ong.service.abstraction.IDeleteSlideService;
+import com.alkemy.ong.service.abstraction.IGetSlideService;
 import com.fasterxml.jackson.databind.introspect.TypeResolutionContext.Empty;
+import java.util.List;
 import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +19,19 @@ public class SlideController {
 
   @Autowired
   private IDeleteSlideService deleteSlideService;
+  @Autowired
+  private IGetSlideService getSlideService;
 
   @DeleteMapping(value = "/slides/{id}")
   public ResponseEntity<Empty> delete(@PathVariable("id") long id) throws EntityNotFoundException {
     deleteSlideService.delete(id);
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+
+  @GetMapping(value = "/slides")
+  public ResponseEntity<List<SlideResponse>> getAll() throws EntityNotFoundException {
+    List<SlideResponse> slides = getSlideService.getAllSlides();
+    return ResponseEntity.ok().body(slides);
   }
 
 }
