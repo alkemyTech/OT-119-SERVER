50f73060877060ccbcfd2ce0fe7bd18d9a01c7e4
Mauricio Ferrari <mauricioferrari239@gmail.com>
2021-12-19 19:34:53 -0300

    Implementación del servicio

--- src/main/java/com/alkemy/ong/service/abstraction/IGetUserService.java
+++ src/main/java/com/alkemy/ong/service/abstraction/IGetUserService.java
@@ -1,9 +1,13 @@
 package com.alkemy.ong.service.abstraction;
 
 import com.alkemy.ong.model.entity.User;
+import com.alkemy.ong.model.request.UserAuthenticationRequest;
+import org.springframework.security.core.Authentication;
 
 public interface IGetUserService {
 
   User getBy(String authorizationHeader);
+
+  Authentication authentication(UserAuthenticationRequest authRequest);
 
 }
