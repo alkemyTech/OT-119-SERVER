b987598d6d831ee3f0868694405a6d6c03f51c7c
Mauricio Ferrari <mauricioferrari239@gmail.com>
2021-12-20 01:03:21 -0300

    OT119-45: Exceptions removed, and names refactored

--- src/main/java/com/alkemy/ong/service/abstraction/IGetSlideService.java
+++ src/main/java/com/alkemy/ong/service/abstraction/IGetSlideService.java
@@ -1,11 +1,9 @@
 package com.alkemy.ong.service.abstraction;
 
-import com.alkemy.ong.model.response.SlideResponse;
-import java.util.List;
-import javax.persistence.EntityNotFoundException;
+import com.alkemy.ong.model.response.ListSlideResponse;
 
 public interface IGetSlideService {
 
-  List<SlideResponse> getAllSlides() throws EntityNotFoundException;
+  ListSlideResponse getAll();
 
 }
