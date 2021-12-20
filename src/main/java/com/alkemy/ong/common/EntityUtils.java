7e9f43ed9f5271aaefd6e3178c6626e9e56d4a4f
Mauricio Ferrari <mauricioferrari239@gmail.com>
2021-12-20 00:57:34 -0300

    OT119-45: Mapper method moved here and refactored its' names

--- src/main/java/com/alkemy/ong/common/EntityUtils.java
+++ src/main/java/com/alkemy/ong/common/EntityUtils.java
@@ -1,7 +1,12 @@
 package com.alkemy.ong.common;
 
 import com.alkemy.ong.model.entity.Organization;
+import com.alkemy.ong.model.entity.Slide;
 import com.alkemy.ong.model.response.OrganizationResponse;
+import com.alkemy.ong.model.response.SlideResponse;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import org.springframework.beans.BeanUtils;
 
 public class EntityUtils {
@@ -14,4 +19,18 @@ public class EntityUtils {
     BeanUtils.copyProperties(organization, organizationResponse);
     return organizationResponse;
   }
+
+  public static List<SlideResponse> convertTo(Collection<Slide> slides) {
+    List<SlideResponse> slideResponseList = new ArrayList<>();
+    SlideResponse slideResponse;
+    for (Slide slide : slides) {
+      slideResponse = new SlideResponse();
+      slideResponse.setImageUrl(slide.getImageUrl());
+      slideResponse.setOrder(slide.getOrder());
+      slideResponseList.add(slideResponse);
+    }
+    return slideResponseList;
+
+  }
+
 }
