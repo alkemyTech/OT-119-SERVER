77d477585bc06f4ba067706a93e439e52e2e06bc
Mauricio Ferrari <mauricioferrari239@gmail.com>
2021-12-20 01:01:48 -0300

    OT119-45 Created ListSlideResponse, and updated application.properties to correct JSON response

--- src/main/java/com/alkemy/ong/controller/SlideController.java
+++ src/main/java/com/alkemy/ong/controller/SlideController.java
@@ -1,10 +1,9 @@
 package com.alkemy.ong.controller;
 
-import com.alkemy.ong.model.response.SlideResponse;
+import com.alkemy.ong.model.response.ListSlideResponse;
 import com.alkemy.ong.service.abstraction.IDeleteSlideService;
 import com.alkemy.ong.service.abstraction.IGetSlideService;
 import com.fasterxml.jackson.databind.introspect.TypeResolutionContext.Empty;
-import java.util.List;
 import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,8 +28,8 @@ public class SlideController {
   }
 
   @GetMapping(value = "/slides")
-  public ResponseEntity<List<SlideResponse>> getAll() throws EntityNotFoundException {
-    List<SlideResponse> slides = getSlideService.getAllSlides();
+  public ResponseEntity<ListSlideResponse> getAll() throws EntityNotFoundException {
+    ListSlideResponse slides = getSlideService.getAll();
     return ResponseEntity.ok().body(slides);
   }
 
