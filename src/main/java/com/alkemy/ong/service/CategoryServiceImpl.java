27f27d148631f80b0935c50e5e65abd1e37cd7ea
Nazareno <laraianazareno@gmail.com>
2021-12-20 16:03:05 -0300

    OT119-32: Add Get implementations, create IGetCategoryService interface

--- src/main/java/com/alkemy/ong/service/CategoryServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/CategoryServiceImpl.java
@@ -1,15 +1,19 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.common.DtoUtils;
+import com.alkemy.ong.common.EntityUtils;
 import com.alkemy.ong.model.entity.Category;
+import com.alkemy.ong.model.response.CategoryDetailsResponse;
 import com.alkemy.ong.repository.ICategoryRepository;
 import com.alkemy.ong.service.abstraction.IDeleteCategoryService;
+import com.alkemy.ong.service.abstraction.IGetCategoryService;
 import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CategoryServiceImpl implements IDeleteCategoryService {
+public class CategoryServiceImpl implements IDeleteCategoryService, IGetCategoryService {
 
   private static final String CATEGORY_NOT_FOUND_MESSAGE = "Category not found.";
 
@@ -21,6 +25,11 @@ public class CategoryServiceImpl implements IDeleteCategoryService {
     Category category = getCategory(id);
     category.setSoftDelete(true);
     categoryRepository.save(category);
+  }
+  @Override
+  public CategoryDetailsResponse findById(Long id){
+    Category category = getCategory(id);
+    return EntityUtils.convertTo(category);
   }
 
   private Category getCategory(Long id) {
