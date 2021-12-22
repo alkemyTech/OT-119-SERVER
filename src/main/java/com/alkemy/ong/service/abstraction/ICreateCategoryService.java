e98e5961a726e89b80c95479232a510534d77933
Nazareno <laraianazareno@gmail.com>
2021-12-22 17:43:33 -0300

    OT119-33: Created CategoryDetailsRequest, ICreateCategoryService

---
+++ src/main/java/com/alkemy/ong/service/abstraction/ICreateCategoryService.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.service.abstraction;
+
+import com.alkemy.ong.model.request.CategoryDetailsRequest;
+
+public interface ICreateCategoryService {
+
+  void createCategory(CategoryDetailsRequest categoryDetailsRequest);
+
+}
