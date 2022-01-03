24d5544fc6398f19bc786f9ee2b77ba3a58beafd
Mauricio Ferrari <mauricioferrari239@gmail.com>
2022-01-02 21:29:36 -0300

    OT119-64: Agregada la query para realizar búsquedas en la base de datos

--- src/main/java/com/alkemy/ong/repository/ICommentRepository.java
+++ src/main/java/com/alkemy/ong/repository/ICommentRepository.java
@@ -1,10 +1,17 @@
 package com.alkemy.ong.repository;
 
 import com.alkemy.ong.model.entity.Comment;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ICommentRepository extends JpaRepository<Comment, Long> {
+
+  @Query(value = "SELECT * FROM comments WHERE news_id = :newsId",
+  nativeQuery = true)
+  List<Comment> findAllCommentsByNewsId(@Param("newsId") Long newsId);
 
 }
