c794021afb9fce77ce7bbe0bc456f59faa0119be
Mauricio Ferrari <mauricioferrari239@gmail.com>
2022-01-04 08:58:39 -0300

    OT119-63: Created PUT endpoint to update comment's body

--- src/main/java/com/alkemy/ong/controller/CommentController.java
+++ src/main/java/com/alkemy/ong/controller/CommentController.java
@@ -1,13 +1,17 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.exception.OperationNotAllowedException;
+import com.alkemy.ong.model.request.CommentRequest;
 import com.alkemy.ong.service.abstraction.IDeleteCommentsService;
+import com.alkemy.ong.service.abstraction.IUpdateCommentService;
 import com.fasterxml.jackson.databind.introspect.TypeResolutionContext.Empty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +20,8 @@ public class CommentController {
 
   @Autowired
   private IDeleteCommentsService deleteCommentsService;
+  @Autowired
+  private IUpdateCommentService updateCommentService;
 
   @DeleteMapping(value = "/comments/{id}")
   public ResponseEntity<Empty> delete(@PathVariable("id") long id,
@@ -25,4 +31,10 @@ public class CommentController {
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 
+  @PutMapping(value = "/comments/{id}")
+  public void updateComment(@PathVariable("id") long id,
+      @RequestBody CommentRequest commentRequest,
+      @RequestHeader(value = "Authorization") String authorizationHeader) {
+    updateCommentService.updateComment(commentRequest.getBody(), id, authorizationHeader);
+  }
 }
