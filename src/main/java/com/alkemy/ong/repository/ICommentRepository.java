d260c73d36eade23da3b8c158e1b5e419edbc353
Mauricio Ferrari <mauricioferrari239@gmail.com>
2022-01-04 09:25:30 -0300

    OT119-63: Updated postman collection

--- src/main/java/com/alkemy/ong/repository/ICommentRepository.java
+++ src/main/java/com/alkemy/ong/repository/ICommentRepository.java
@@ -3,9 +3,11 @@ package com.alkemy.ong.repository;
 import com.alkemy.ong.model.entity.Comment;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ICommentRepository extends JpaRepository<Comment, Long> {
@@ -14,4 +16,8 @@ public interface ICommentRepository extends JpaRepository<Comment, Long> {
   nativeQuery = true)
   List<Comment> findAllCommentsByNewsId(@Param("newsId") Long newsId);
 
+  @Transactional
+  @Modifying
+  @Query(value = "update Comment u set u.body = :body where u.id = :id")
+  void updateComment(@Param(value = "body") String body, @Param(value = "id") long id);
 }
