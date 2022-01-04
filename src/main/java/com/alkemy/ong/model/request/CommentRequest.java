204899d5eca8aac5246265380050e212c1f98ac0
Mauricio Ferrari <mauricioferrari239@gmail.com>
2022-01-04 09:26:39 -0300

    OT119-63: Created CommentRequest model

---
+++ src/main/java/com/alkemy/ong/model/request/CommentRequest.java
@@ -0,0 +1,16 @@
+package com.alkemy.ong.model.request;
+
+import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentRequest {
+
+  private String body;
+}
