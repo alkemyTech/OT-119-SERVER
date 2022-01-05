fc9f60a41e382ee2f0c6cd22b848e541153f6fb0
Pedro Asán Martínez <asanmartinezp>
2022-01-05 17:34:43 -0300

    OT119-30: Code clean-up

--- src/main/java/com/alkemy/ong/common/ImageUtils.java
+++ src/main/java/com/alkemy/ong/common/ImageUtils.java
@@ -2,13 +2,12 @@ package com.alkemy.ong.common;
 
 import com.alkemy.ong.common.mail.EmailUtils;
 import com.alkemy.ong.config.AmazonClient;
-import com.amazonaws.AmazonClientException;
+import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import java.io.InputStream;
-import java.net.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,10 +32,10 @@ public class ImageUtils {
       request.setCannedAcl(CannedAccessControlList.PublicRead);
       amazonS3.putObject(request);
     } catch (
-        AmazonClientException e) {
+        SdkClientException e) {
       LOGGER.error(e.getMessage());
     }
-    URL objectUrl = amazonS3.getUrl(amazonClient.getBucketName(), fileName);
-    return objectUrl.toExternalForm();
+    String url = amazonS3.getUrl(amazonClient.getBucketName(), fileName).toExternalForm();
+    return url;
   }
 }
