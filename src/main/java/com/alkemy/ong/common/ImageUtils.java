9d98f7547e860bac0742644cd7a625b8945169e8
Pedro Asán Martínez <94468975+asanmartinezp@users.noreply.github.com>
2022-01-06 12:04:54 -0300

    OT119-30: implemented utility class to upload images to the Amazon S3 service (#25)

---
+++ src/main/java/com/alkemy/ong/common/ImageUtils.java
@@ -0,0 +1,44 @@
+package com.alkemy.ong.common;
+
+import com.alkemy.ong.common.mail.EmailUtils;
+import com.alkemy.ong.config.AmazonClient;
+import com.alkemy.ong.exception.CustomException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.InputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageUtils {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(EmailUtils.class);
+
+  @Autowired
+  private AmazonClient amazonClient;
+  @Autowired
+  private AmazonS3 amazonS3;
+
+  public String upload(InputStream inputStream, String fileName, String contentType)
+      throws CustomException {
+    try {
+      ObjectMetadata metadata = new ObjectMetadata();
+      metadata.setContentType(contentType);
+      PutObjectRequest request = new PutObjectRequest(amazonClient.getBucketName(), fileName,
+          inputStream, metadata);
+      request.setCannedAcl(CannedAccessControlList.PublicRead);
+      amazonS3.putObject(request);
+    } catch (
+        SdkClientException e) {
+      LOGGER.error(e.getMessage());
+      throw new CustomException(
+          "There was an internal error with the Amazon S3 service. Please contact the support or try again later.");
+    }
+    return amazonS3.getUrl(amazonClient.getBucketName(), fileName).toExternalForm();
+  }
+}
