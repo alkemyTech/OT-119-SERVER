26e14a75980c3c436c6772f94415ed60b1db3ca0
Mauricio Ferrari <mauricioferrari239@gmail.com>
2022-01-08 19:05:00 -0300

    Created methods on the service layer for pagination

--- src/main/java/com/alkemy/ong/service/abstraction/IGetCategoryService.java
+++ src/main/java/com/alkemy/ong/service/abstraction/IGetCategoryService.java
@@ -1,12 +1,22 @@
 package com.alkemy.ong.service.abstraction;
 
+import com.alkemy.ong.model.entity.Category;
 import com.alkemy.ong.model.response.CategoryDetailsResponse;
 import com.alkemy.ong.model.response.ListCategoryResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.util.UriComponentsBuilder;
 
 public interface IGetCategoryService {
 
   CategoryDetailsResponse findBy(Long id);
 
   ListCategoryResponse list();
+
+  Page<Category> findPaginated(Pageable pageable);
+
+  void addLinksToHeader(UriComponentsBuilder uriBuilder, int page, int totalPages, int pageSize,
+      HttpServletResponse response);
 
 }
