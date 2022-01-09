b4b5499e88a12dcb3cd85f42fd97b4c6dd2bf261
Magali Kain <magali.kain@thoughtworks.com>
2022-01-09 13:32:03 +0100

    OT119-27: apply right formatter in UserServiceImpl

--- src/main/java/com/alkemy/ong/service/UserServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/UserServiceImpl.java
@@ -18,6 +18,10 @@ import com.alkemy.ong.service.abstraction.IDeleteUserService;
 import com.alkemy.ong.service.abstraction.IGetUserService;
 import com.alkemy.ong.service.abstraction.IRegisterUserService;
 import com.alkemy.ong.service.abstraction.IRoleService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -26,11 +30,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import javax.persistence.EntityNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserDetailsService, IDeleteUserService, IGetUserService,
