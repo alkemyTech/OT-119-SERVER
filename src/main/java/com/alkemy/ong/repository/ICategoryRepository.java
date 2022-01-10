59b53d5f88319779b4f1796448dcfcb7ec390829
Mauricio Ferrari <mauricioferrari239@gmail.com>
2022-01-10 08:39:58 -0300

    OT119-65: deleted multi heritage on this interface

--- src/main/java/com/alkemy/ong/repository/ICategoryRepository.java
+++ src/main/java/com/alkemy/ong/repository/ICategoryRepository.java
@@ -4,13 +4,10 @@ import com.alkemy.ong.model.entity.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ICategoryRepository extends JpaRepository<Category, Long>,
-    PagingAndSortingRepository<Category, Long> {
+public interface ICategoryRepository extends JpaRepository<Category, Long> {
 
-  @Override
-  Page<Category> findAll(Pageable pageable);
+  Page<Category> findBySoftDeleteFalse(Pageable pageable);
 }
