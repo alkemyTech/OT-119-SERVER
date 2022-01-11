5b92bba04b1b486c2321eec960598e0e8c58f611
Nazareno <laraianazareno@gmail.com>
2022-01-11 02:42:31 -0300

    OT119-81: Implements Login user integration test

---
+++ src/test/java/com/alkemy/ong/integration/user/LoginUserIntegrationTest.java
@@ -0,0 +1,69 @@
+package com.alkemy.ong.integration.user;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+import com.alkemy.ong.common.AbstractBaseIntegrationTest;
+import com.alkemy.ong.common.JwtUtils;
+import com.alkemy.ong.config.security.ApplicationRole;
+import com.alkemy.ong.model.entity.User;
+import com.alkemy.ong.model.request.UserAuthenticationRequest;
+import com.alkemy.ong.model.response.ErrorResponse;
+import com.alkemy.ong.model.response.UserAuthenticatedResponse;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LoginUserIntegrationTest extends AbstractBaseIntegrationTest {
+
+  private final String PATH = "/auth/login";
+  @MockBean
+  private JwtUtils jwtUtils;
+
+  @Test
+  public void shouldLoginUserSuccessfully() {
+    User user = stubUser(ApplicationRole.USER.getFullRoleName());
+    when(userRepository.findByEmail(eq("johnny@gmail.com"))).thenReturn(user);
+    UserAuthenticationRequest userAuthenticationRequest = getUserAuthenticationRequest();
+    when(jwtUtils.generateToken(eq(user))).thenReturn(user.getEmail());
+
+    ResponseEntity<UserAuthenticatedResponse> response = restTemplate.exchange(
+        createURLWithPort(PATH),
+        HttpMethod.POST,
+        new HttpEntity<>(userAuthenticationRequest, headers),
+        UserAuthenticatedResponse.class);
+
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+
+  @Test
+  public void shouldUserNotExist() {
+    User user = stubUser(ApplicationRole.USER.getFullRoleName());
+    UserAuthenticationRequest userAuthenticationRequest = getUserAuthenticationRequest();
+    when(userRepository.findByEmail(eq("johnny@gmail.com"))).thenReturn(null);
+
+    ResponseEntity<ErrorResponse> response = restTemplate.exchange(
+        createURLWithPort(PATH),
+        HttpMethod.POST,
+        new HttpEntity<>(userAuthenticationRequest, headers),
+        ErrorResponse.class);
+
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+  }
+
+  private UserAuthenticationRequest getUserAuthenticationRequest() {
+    UserAuthenticationRequest userAuthenticationRequest = new UserAuthenticationRequest();
+    userAuthenticationRequest.setEmail("johnny@gmail.com");
+    userAuthenticationRequest.setPassword("123456789");
+    return userAuthenticationRequest;
+  }
+}
