bbb1af4d23e4dbaa8a38d66f08f8e8a38729db6f
Pedro Asán Martínez <asanmartinezp>
2022-01-11 12:59:53 -0300

    OT119-77: Implemented the test according to examples.

--- src/test/java/com/alkemy/ong/integration/organization/OrganizationPublicEndpointIntegrationTest.java
+++ src/test/java/com/alkemy/ong/integration/organization/OrganizationPublicEndpointIntegrationTest.java
@@ -1,48 +1,37 @@
 package com.alkemy.ong.integration.organization;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.Assert.assertEquals;
 
-import com.alkemy.ong.common.JwtUtils;
-import com.alkemy.ong.controller.OrganizationController;
-import com.alkemy.ong.model.response.OrganizationResponse;
+import com.alkemy.ong.common.AbstractBaseIntegrationTest;
 import com.alkemy.ong.service.OrganizationServiceImpl;
-import com.alkemy.ong.service.UserServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@WebMvcTest(OrganizationController.class)
+
 @RunWith(SpringRunner.class)
-public class OrganizationPublicEndpointIntegrationTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class OrganizationPublicEndpointIntegrationTest extends AbstractBaseIntegrationTest {
 
   private final String PATH = "/organization/public";
-
-  @Autowired
-  MockMvc mockMvc;
 
   @MockBean
   OrganizationServiceImpl organizationService;
 
-  @MockBean
-  UserServiceImpl userService;
-
-  @MockBean
-  JwtUtils jwtUtils;
-
   @Test
   public void shouldReturnOkWhenAccessedWithoutRole() throws Exception {
-    Mockito.when(organizationService.getOrganizationWithSlides())
-        .thenReturn(new OrganizationResponse());
-    mockMvc.perform(MockMvcRequestBuilders
-            .get(PATH)
-            .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk());
+    ResponseEntity<Object> response = restTemplate.exchange(
+        createURLWithPort(PATH),
+        HttpMethod.GET,
+        new HttpEntity<>(headers),
+        Object.class);
+
+    assertEquals(response.getStatusCode(), HttpStatus.OK);
   }
 }
