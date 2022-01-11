bcb7a8edfd81bf05349b2a4a8b2801fa0f5a9371
Pedro Asán Martínez <asanmartinezp>
2022-01-11 20:51:10 -0300

    OT119-77: Reworked test.

--- src/test/java/com/alkemy/ong/integration/organization/OrganizationPublicEndpointIntegrationTest.java
+++ src/test/java/com/alkemy/ong/integration/organization/OrganizationPublicEndpointIntegrationTest.java
@@ -1,11 +1,22 @@
 package com.alkemy.ong.integration.organization;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import com.alkemy.ong.common.AbstractBaseIntegrationTest;
-import com.alkemy.ong.service.OrganizationServiceImpl;
+import com.alkemy.ong.model.entity.Organization;
+import com.alkemy.ong.model.entity.Slide;
+import com.alkemy.ong.repository.IOrganizationRepository;
+import com.alkemy.ong.repository.ISlideRepository;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpEntity;
@@ -22,16 +33,76 @@ public class OrganizationPublicEndpointIntegrationTest extends AbstractBaseInteg
   private final String PATH = "/organization/public";
 
   @MockBean
-  OrganizationServiceImpl organizationService;
+  private ISlideRepository slideRepository;
+
+  @MockBean
+  private IOrganizationRepository organizationRepository;
+
+  private ArrayList<Organization> organizationsStub = new ArrayList<>();
+  private List<Slide> slidesStub = new ArrayList<>();
+
+  private void setupStubs() {
+    Organization organization = new Organization();
+    organization.setAboutUsText("About Us");
+    organization.setImage("http://foo.png");
+    organization.setAddress("Calle 123");
+    organization.setEmail("somos.mas@outlook.com");
+    organization.setPhone(456);
+    organization.setTimeStamp(Timestamp.from(Instant.now()));
+    organizationsStub.add(organization);
+
+    Slide slide1 = new Slide();
+    slide1.setImageUrl("/foo.png");
+    slide1.setText("Slide 1 text");
+    slide1.setOrder(1);
+
+    Slide slide2 = new Slide();
+    slide2.setImageUrl("/bar.jpg");
+    slide2.setText("Slide 2 text");
+    slide2.setOrder(2);
+
+    slidesStub.add(slide1);
+    slidesStub.add(slide2);
+  }
 
   @Test
-  public void shouldReturnOkWhenAccessedWithoutRole() throws Exception {
+  public void shouldReturnOkWhenAccessedWithoutRole() {
+    setupStubs();
+
+    Mockito.when(organizationRepository.findAll()).thenReturn(organizationsStub);
+    Mockito.when(slideRepository.findAll()).thenReturn(slidesStub);
+
     ResponseEntity<Object> response = restTemplate.exchange(
         createURLWithPort(PATH),
         HttpMethod.GET,
         new HttpEntity<>(headers),
         Object.class);
 
-    assertEquals(response.getStatusCode(), HttpStatus.OK);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    LinkedHashMap responseBody = (LinkedHashMap) response.getBody();
+    assertNotNull(responseBody);
+    assertEquals(organizationsStub.get(0).getEmail(), responseBody.get("email"));
+    assertEquals(organizationsStub.get(0).getImage(), responseBody.get("image"));
+    assertEquals(organizationsStub.get(0).getAddress(), responseBody.get("address"));
+    assertEquals(organizationsStub.get(0).getPhone(), responseBody.get("phone"));
+
+//    HashMap slide1 = (HashMap) ((List) responseBody.get("slides")).get(0);
+//    HashMap slide2 = (HashMap) ((List) responseBody.get("slides")).get(1);
+//
+//    assertEquals(slidesStub.get(0).getImageUrl(), slide1.get("imageUrl"));
+//    assertEquals(slidesStub.get(0).getOrder(), slide1.get("order"));
+//
+//    assertEquals(slidesStub.get(1).getImageUrl(), slide2.get("imageUrl"));
+//    assertEquals(slidesStub.get(1).getOrder(), slide2.get("order"));
+
+    List slidesResponse = (List) responseBody.get("slides");
+
+    assertEquals(slidesStub.size(), slidesResponse.size());
+
+    for (int i = 0; i < slidesResponse.size(); i++) {
+      HashMap slideAttributResponse = ((HashMap) slidesResponse.get(i));
+      assertEquals(slidesStub.get(i).getImageUrl(), slideAttributResponse.get("imageUrl"));
+      assertEquals(slidesStub.get(i).getOrder(), slideAttributResponse.get("order"));
+    }
   }
 }
