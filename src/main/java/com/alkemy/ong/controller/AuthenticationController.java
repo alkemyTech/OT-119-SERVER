0308a1b9c220dc0692c7c95c410ca93201fdbbf7
Nazareno <laraianazareno@gmail.com>
2022-01-11 02:41:11 -0300

    OT119-81: Moved logic from controller to ServiceImpl

--- src/main/java/com/alkemy/ong/controller/AuthenticationController.java
+++ src/main/java/com/alkemy/ong/controller/AuthenticationController.java
@@ -1,18 +1,14 @@
 package com.alkemy.ong.controller;
 
-import com.alkemy.ong.common.mail.template.EmailAddress;
-import com.alkemy.ong.exception.SendEmailException;
+
 import com.alkemy.ong.exception.UserAlreadyExistException;
 import com.alkemy.ong.model.request.UserAuthenticationRequest;
 import com.alkemy.ong.model.request.UserDetailsRequest;
 import com.alkemy.ong.model.response.UserAuthenticatedResponse;
 import com.alkemy.ong.model.response.UserDetailsResponse;
-import com.alkemy.ong.service.WelcomeEmailService;
 import com.alkemy.ong.service.abstraction.IAuthenticationService;
 import com.alkemy.ong.service.abstraction.IRegisterUserService;
 import javax.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,14 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthenticationController {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationController.class);
 
   @Autowired
   private IRegisterUserService registerUserService;
   @Autowired
   private IAuthenticationService authenticationService;
-  @Autowired
-  private WelcomeEmailService welcomeEmailService;
 
   @PostMapping("/login")
   public ResponseEntity<UserAuthenticatedResponse> login(
@@ -45,14 +38,6 @@ public class AuthenticationController {
       @Valid @RequestBody UserDetailsRequest userDetailsRequest)
       throws UserAlreadyExistException {
     UserDetailsResponse userDetailsResponse = registerUserService.register(userDetailsRequest);
-    EmailAddress toEmailAddress = new EmailAddress(userDetailsResponse.getEmail(),
-        String.format("%s %s", userDetailsRequest.getFirstName(),
-            userDetailsRequest.getLastName()));
-    try {
-      welcomeEmailService.sendEmail(toEmailAddress);
-    } catch (SendEmailException e) {
-      LOGGER.error(e.getMessage());
-    }
     return ResponseEntity.status(HttpStatus.CREATED).body(userDetailsResponse);
   }
 
